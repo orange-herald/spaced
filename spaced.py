ac12f8f2456c35635ba85a56234c85fb3850ba2f
Steven MacDiarmid <stevenmacdiarmid@me.com>
2018-04-12 17:02:50 +0100

    Changed calculation from disk used to free space

--- spaced.py
+++ spaced.py
@@ -6,7 +6,12 @@ Server disk space monitor/alert
 :copyright 2018 QueryClick all rights reserved
 """
 
+import datetime
 import subprocess
+
+
+REMAINING_DISK_SPACE_LIMIT = 25
+
 
 def get_free_disk_space():
     # Shell command to display disk usage for / mount point
@@ -14,22 +19,28 @@ def get_free_disk_space():
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     free_space = process.stdout.read()
     free_space = free_space.split('\n')[1][:-1]
-    return free_space
-
-#print "Free space {} %".format(free_space)
-
-# if free_space < WARN_THRESHOLD
-    # log value
-    # do nothing
+    return 100 - int(free_space)
 
 
-# if free_space >= WARN_THRESHOLD
-    # send email
-    # log value
+def log_data(free_space, disk_status):
+    timestamp = datetime.datetime.now()
+    print "{:%Y-%m-%d %H:%M:%S} : FREE_DISK SPACE: {}%. STATUS: {}.".format(timestamp, free_space, disk_status)
+
+
+def send_low_disk_alert():
+    print 'Sad face!'
+    
 
 
 def main():
-    print get_free_disk_space()
+    free_space = get_free_disk_space()
+    # print free_space
+
+    if free_space > REMAINING_DISK_SPACE_LIMIT:
+        log_data(free_space, 'GOOD')
+    else:
+        log_data(free_space, 'BAD')
+        send_low_disk_alert()
 
 
 if __name__ == "__main__":
