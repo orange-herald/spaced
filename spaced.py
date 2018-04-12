2cc2a6527a41288c858ad6c354f024a933511913
Steven MacDiarmid <stevenmacdiarmid@me.com>
2018-04-12 15:59:04 +0100

    Returning free disk space % correctly

--- spaced.py
+++ spaced.py
@@ -1,12 +1,22 @@
+#! /env/python
+
 """
 Server disk space monitor/alert
 :author Steven MacDiarmid
 :copyright 2018 QueryClick all rights reserved
 """
 
-# df -h / | tail -1 | awk '{ print $5 }'
+import subprocess
 
-# run disk space command
+def get_free_disk_space():
+    # Shell command to display disk usage for / mount point
+    command = "findmnt / -DoUSE%".split()
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    free_space = process.stdout.read()
+    free_space = free_space.split('\n')[1][:-1]
+    return free_space
+
+#print "Free space {} %".format(free_space)
 
 # if free_space < WARN_THRESHOLD
     # log value
@@ -16,3 +26,11 @@ Server disk space monitor/alert
 # if free_space >= WARN_THRESHOLD
     # send email
     # log value
+
+
+def main():
+    print get_free_disk_space()
+
+
+if __name__ == "__main__":
+    main()
